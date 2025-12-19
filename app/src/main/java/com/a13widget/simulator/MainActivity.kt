e89882f3afbaa9c6918b43348f0214e26ad07aab
RioChx <73mahy@gmail.com>
2025-12-19 12:11:34 +1100

    Add files via upload

---
+++ app/src/main/java/com/a13widget/simulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.a13widget.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override function onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
